7f4240315de835220e8e66b89c604f103dd5cfc1
Ayazg <ayaz0206@gmail.com>
2014-02-28 15:08:43 +0000

    5 done!

---
+++ README.md
@@ -0,0 +1,4 @@
+This is the test for Week 8.
+
+There are a bunch of questions to revisit the basics of Ruby.
+
---
+++ questions.rb
@@ -0,0 +1,196 @@
+# Questions + Answers. Tests are located in spec directory.
+
+# keep only the elements that start with an a
+def select_elements_starting_with_a(letter)
+	letter.select {|x| x.start_with? 'a' } 
+end
+
+# keep only the elements that start with a vowel
+def select_elements_starting_with_vowel(letter)
+	letter.select {|x|  x =~ (/^[aeiou]/)}
+end
+
+# remove instances of nil (but NOT false) from an array
+def remove_nils_from_array(array)
+	array.reject {|x| x.nil? }
+end
+
+# remove instances of nil AND false from an array
+def remove_nils_and_false_from_array(array)
+	array.reject {|x| x == false || x.nil? }
+end
+
+# don't reverse the array, but reverse every word inside it. e.g.
+# ['dog', 'monkey'] becomes ['god', 'yeknom']
+def reverse_every_element_in_array(array)
+	array.map.each {|x| x.reverse!}
+end
+
+# given an array of student names, like ['Bob', 'Dave', 'Clive']
+# give every possible pairing - in this case:
+# [['Bob', 'Clive'], ['Bob', 'Dave'], ['Clive', 'Dave']]
+# make sure you don't have the same pairing twice, 
+
+
+# discard the first 3 elements of an array, 
+# e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
+
+
+# add an element to the beginning of an array
+
+
+# sort an array of words by their last letter, e.g.
+# ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
+
+
+# cut strings in half, and return the first half, e.g.
+# 'banana' becomes 'ban'. If the string is an odd number of letters
+# round up - so 'apple' becomes 'app'
+
+
+# turn a positive integer into a negative integer. A negative integer
+# stays negative
+
+
+# turn an array of numbers into two arrays of numbers, one an array of 
+# even numbers, the other an array of odd numbers
+# even numbers come first
+# so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
+
+
+# count the numbers of elements in an element which are palindromes
+# a palindrome is a word that's the same backwards as forward
+# e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
+# are 2 palindromes (bob and radar), so the method should return 2
+
+
+# return the shortest word in an array
+
+
+# return the longest word in an array
+
+
+# add up all the numbers in an array, so [1, 3, 5, 6]
+# returns 15
+
+
+# turn an array into itself repeated twice. So [1, 2, 3]
+# becomes [1, 2, 3, 1, 2, 3]
+
+
+# convert a symbol into a string
+
+
+# get the average from an array, rounded to the nearest integer
+# so [10, 15, 25] should return 33
+
+
+# get all the elements in an array, up until the first element
+# which is greater than five. e.g.
+# [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
+# becomes [1, 3, 5, 4, 1, 2]
+
+
+# turn an array (with an even number of elements) into a hash, by
+# pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
+# {'a' => 'b', 'c' => 'd'}
+
+
+# get all the letters used in an array of words and return
+# it as a array of letters, in alphabetical order
+# . e.g. the array ['cat', 'dog', 'fish'] becomes
+# ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
+
+
+# swap the keys and values in a hash. e.g.
+# {'a' => 'b', 'c' => 'd'} becomes
+# {'b' => 'a', 'd' => 'c'}
+
+
+# in a hash where the keys and values are all numbers
+# add all the keys and all the values together, e.g.
+# {1 => 1, 2 => 2} becomes 6
+
+
+# take out all the capital letters from a string
+# so 'Hello JohnDoe' becomes 'ello ohnoe'
+
+
+# round up a float up and convert it to an Integer,
+# so 3.214 becomes 4
+
+
+# round down a float up and convert it to an Integer,
+# so 9.52 becomes 9
+
+
+# take a date and format it like dd/mm/yyyy, so Halloween 2013
+# becomes 31/10/2013
+
+
+# get the domain name *without* the .com part, from an email address
+# so alex@makersacademy.com becomes makersacademy
+
+
+# capitalize the first letter in each word of a string, 
+#  except 'a', 'and' and 'the'
+# *unless* they come at the start of the start of the string, e.g.
+# 'the lion the witch and the wardrobe' becomes
+# 'The Lion the Witch and the Wardrobe'
+
+
+# return true if a string contains any special characters
+# where 'special character' means anything apart from the letters
+# a-z (uppercase and lower) or numbers
+
+
+# get the upper limit of a range. e.g. for the range 1..20, you
+# should return 20
+
+
+# should return true for a 3 dot range like 1...20, false for a 
+# normal 2 dot range
+
+
+# get the square root of a number
+
+
+# count the number of words in a file
+
+
+# --- tougher ones ---
+
+# call an arbitrary method from a string. so if I
+# called call_method_from_string('foobar')
+# the method foobar should be invoked
+
+
+# return true if the date is a uk bank holiday for 2014
+# the list of bank holidays is here:
+# https://www.gov.uk/bank-holidays
+
+
+# given your birthday this year, this method tells you
+# the next year when your birthday will fall on a friday
+# e.g. january 1st, will next be a friday in 2016
+# return the day as a capitalized string like 'Friday'
+
+
+# in a file, total the number of times words of different lengths
+# appear. So in a file with the text "the cat sat on the blue mat"
+# I have 5 words which are 3 letters long, 1 which is 2 letters long
+# and 1 that is 4 letters long. Return it as a hash in the format
+# word_length => count, e.g. {2 => 1, 3 => 5, 4 => 1}
+
+
+# implement fizzbuzz without modulo, i.e. the % method
+# go from 1 to 100
+# (there's no RSpec test for this one)
+
+
+# print the lyrics of the song 99 bottles of beer on the wall
+# http://www.99-bottles-of-beer.net/lyrics.html
+# make sure you use the singular when you have one bottle of 
+# beer on the wall, and print 'no more bottles of beer on the wall'
+# at the end.
+# (there's no RSpec test for this one)
