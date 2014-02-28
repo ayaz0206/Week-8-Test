015c4a56d75abe62ce27dee18266c876d1f894d2
Ayazg <ayaz0206@gmail.com>
2014-02-28 16:29:11 +0000

    Got many more done!

--- questions.rb
+++ questions.rb
@@ -30,77 +30,107 @@ end
 # give every possible pairing - in this case:
 # [['Bob', 'Clive'], ['Bob', 'Dave'], ['Clive', 'Dave']]
 # make sure you don't have the same pairing twice, 
-
+def every_possible_pairing_of_students(array)
+	array.combination(2)
+end
 
 # discard the first 3 elements of an array, 
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
-
+def all_elements_except_first_3(array)
+	array.drop(3)
+end
 
 # add an element to the beginning of an array
-
+def add_element_to_beginning_of_array(array, number)
+	array.insert(0, number)
+end
 
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
-
+def array_sort_by_last_letter_of_word(array)
+	array.sort_by! { |x| x.chars.last } 
+end
 
 # cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
-
+def get_first_half_of_string
+end
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
-
+def make_numbers_negative(n)
+	-n.abs
+end
 
 # turn an array of numbers into two arrays of numbers, one an array of 
 # even numbers, the other an array of odd numbers
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
-
+def separate_array_into_even_and_odd_numbers(array)
+	array.partition { |x| x.even? }
+end
 
 # count the numbers of elements in an element which are palindromes
 # a palindrome is a word that's the same backwards as forward
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
-
+def number_of_elements_that_are_palindromes(array)
+	array.select {|x| x == x.reverse}.length
+end
 
 # return the shortest word in an array
-
+def shortest_word_in_array
+end
 
 # return the longest word in an array
-
+def longest_word_in_array
+end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
-
+def total_of_array(array)
+	array.inject {|sum,x| sum + x }
+end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
-
+def double_array(array)
+	array * 2
+end
 
 # convert a symbol into a string
-
+def turn_symbol_into_string(symbol)
+	symbol.to_s
+end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 33
-
+def average_of_array(array)
+	(array.inject{ |sum, element| sum + element }.to_f / array.size).round
+end
 
 # get all the elements in an array, up until the first element
 # which is greater than five. e.g.
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
+def get_elements_until_greater_than_five
 
+end
 
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
-
+def convert_array_to_a_hash(array)
+	Hash[*array.flatten]
+end
 
 # get all the letters used in an array of words and return
 # it as a array of letters, in alphabetical order
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
-
+def get_all_letters_in_array_of_words
+end
 
 # swap the keys and values in a hash. e.g.
 # {'a' => 'b', 'c' => 'd'} becomes
